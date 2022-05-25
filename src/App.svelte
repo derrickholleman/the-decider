ef7b64d340d0fe1d02324ce389606f561a00be84
Derrick Holleman <derrickholleman@gmail.com>
2022-05-24 22:48:49 -0500

    color scheme stuff

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,10 @@
   };
 </script>
 
+<svelte:head>
+  <title>The Decider</title>
+</svelte:head>
+
 <div class="page-wrapper">
   <h1>The Decider</h1>
   <select
@@ -37,14 +41,36 @@
 </div>
 
 <style>
+  h1 {
+    font-size: clamp(2rem, 5vw, 6rem);
+    color: #08415c;
+  }
   .page-wrapper {
     text-align: center;
   }
   button {
     cursor: pointer;
+    margin-left: 1rem;
+    padding: 0.5rem 1rem;
+    color: white;
+    background-color: #709255;
+    border-color: #709255;
+    border-radius: 5px;
+    font-weight: bold;
+  }
+  button:active {
+    transform: scale(0.98);
+  }
+  button:hover {
+    background-color: #81a762;
+    border-color: #81a762;
   }
   .selected-option {
-    margin-top: 2rem;
+    margin-top: 1.5rem;
     font-size: 1.5rem;
+    color: #08415c;
+    font-weight: 600;
   }
+
+  /* #08415c */
 </style>
